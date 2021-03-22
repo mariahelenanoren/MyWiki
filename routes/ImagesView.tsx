2fc3ed283fbf32ea11d0f5aafee75182c61dd710
mariahelenanoren <marialinder97@hotmail.com>
2021-03-22 17:52:53 +0100

    images can be reloaded

--- routes/ImagesView.tsx
+++ routes/ImagesView.tsx
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, createRef } from "react";
 import {
   Image,
-  StyleSheet,
   useWindowDimensions,
   ScrollView,
+  StyleSheet,
 } from "react-native";
+import MainButton from "../components/MainButton";
 import { getImages } from "../helper";
 
 interface ImageSizes {
@@ -17,35 +18,46 @@ interface ImageUrl {
 export default function ImageView() {
   const windowWidth = useWindowDimensions().width;
   const [data, setData] = useState<ImageUrl[]>([]);
+  const [pageCounter, setPageCounter] = useState(1);
+  const scrollRef = createRef<ScrollView>();
 
   useEffect(() => {
     const dataResponse = async () => {
-      const response = await getImages("sunset");
+      const response = await getImages("sunset", pageCounter);
       setData(response);
     };
     dataResponse();
-  }, []);
+  }, [pageCounter]);
 
   return (
-    <ScrollView contentContainerStyle={styles.imageContainer}>
+    <ScrollView ref={scrollRef} contentContainerStyle={styles.imageContainer}>
       {data.map((image) => (
         <Image
           key={image.urls.small}
-          style={{ ...styles.image, width: windowWidth / 2 }}
+          style={{ ...styles.image, width: windowWidth / 2 - 40 }}
           source={{ uri: image.urls.small }}
         ></Image>
       ))}
+      <MainButton
+        title="Load more images"
+        handlePress={() => (
+          setPageCounter(pageCounter + 1),
+          scrollRef.current?.scrollTo({ y: 0, animated: false })
+        )}
+      />
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   image: {
-    height: 200,
+    height: 150,
     resizeMode: "cover",
+    marginBottom: 10,
   },
   imageContainer: {
     display: "flex",
+    justifyContent: "space-between",
     flexWrap: "wrap",
     flexDirection: "row",
     backgroundColor: "#ffffff",
